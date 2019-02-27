114dd3dd88b52e2d3297058fa51879667507116c
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-27 20:54:52 +0300

    Finish homework 5

--- homeworks/5/homework5.js
+++ homeworks/5/homework5.js
@@ -1,39 +1,112 @@
 
+function draw() {
 
-var x = 8;
-var y = 8;
+    var table = document.createElement('table'); // создаем таблицу
+    var flag = true;
 
-function chessBoard (x,y) {
+    var trr = document.createElement('tr'); // создаем строку
+    var arr = [' ','A','B','C','D','E','F','G','H']; // массив с буквами
 
-    var chessBoard = document.getElementById('chessBoard');
+    // генерим ячейки
+    for(var i = 0; i < 8; i++){
+        var tr = document.createElement('tr'); // создаем строку
 
-    for (var i = 0; i < y; i++){
+        // создаем ячейку для цифр
+        var tdd = document.createElement('td');
+        tdd.style.width = '50px';
+        tdd.style.height = '50px';
+        tdd.innerHTML = 8 - i;
+        tdd.style.textAlign = 'center';
+        tr.appendChild(tdd); // добавляем ячейку
 
-        var $row = chessBoard.appendChild(document.createElement('div'));
-        console.log('i: ' + i);
+        //console.log('i: ' + i);
 
-        $row.textContent = y-i; // выводим цифры
+        // генерим доску
+        for(var j = 0; j < 8; j++){
 
+            if(j === 0) { flag = !flag; }
 
-        for (var j = 0; j < x; j++){
+            var td = document.createElement('td'); // создаем яейку
+            //console.log('j: ' + j);
 
-            $row.appendChild(document.createElement("span"));
-            console.log('j: ' + j);
+            td.style.width = '50px';
+            td.style.height = '50px';
 
-            //$row.textContent = x-j; // выводим буквы
-
-            // если первый столбец
-            if (j === 0){
-
+            if(flag){
+                td.style.background = 'black';
+            }else {
+                td.style.background = 'white';
             }
 
-        }
+            td.style.textAlign = 'center';
 
+            // Черные сверху
+            // восьмая строка
+            if (i === 0) {
+                td.style.color = 'black';
+                if ((j === 0) || (j === 7)) {
+                    td.innerHTML = 'Ладья';
+                } else if ((j === 1) || (j === 6)){
+                    td.innerHTML = 'Конь';
+                } else if ((j === 2) || (j === 5)){
+                    td.innerHTML = 'Офицер';
+                } else if (j === 3){
+                    td.innerHTML = 'Ферзь';
+                } else if (j === 4) {
+                    td.innerHTML = 'Король';
+                }
+            }else if(i === 1){
+                td.style.color = 'black';
+                td.innerHTML = 'Пешка';
+            }
+
+
+            // Белые снизу
+            // первая строка
+            if (i === 7) {
+                td.style.color = 'white';
+                if ((j === 0) || (j === 7)) {
+                    td.innerHTML = 'Ладья';
+                } else if ((j === 1) || (j === 6)){
+                    td.innerHTML = 'Конь';
+                } else if ((j === 2) || (j === 5)){
+                    td.innerHTML = 'Офицер';
+                } else if (j === 3){
+                    td.innerHTML = 'Ферзь';
+                } else if (j === 4) {
+                    td.innerHTML = 'Король';
+                }
+            } else if (i === 6){
+                td.style.color = 'white';
+                td.innerHTML = 'Пешка';
+            }
+
+            tr.appendChild(td); // добавляем ячейку
+            flag = !flag; // меняем цвет
+        }
+        table.appendChild(tr);
     }
 
+
+    // этот блок генерит буквы после основной генерации доски
+    for(var k = 0; k < 9; k++){
+
+        var td = document.createElement('td');
+
+        td.style.width = '50px';
+        td.style.height = '50px';
+        td.style.background = 'white';
+        td.style.textAlign = 'center';
+        td.innerHTML = arr[k];
+
+        trr.appendChild(td);
+
+    }
+    table.appendChild(trr);
+
+    document.body.appendChild(table);
 }
 
-
-chessBoard(x,y);
+draw();
 
 
